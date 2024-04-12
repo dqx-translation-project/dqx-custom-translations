cf42d070eab27c1852daaaa4a4618405cfe0c36f
Joey <17505625+jmctune@users.noreply.github.com>
2024-04-11 21:35:08 -0500

    ci: verify that all records in the glossary have a key:value pair. (#150)

--- .github/workflows/checks.py
+++ .github/workflows/checks.py
@@ -4,33 +4,41 @@ import os
 import sys
 
 
-
 def check_glossary():
+    """Parses the glossary.csv file for problems. Fails if any found."""
     with open("csv/glossary.csv", "r", encoding="utf-8") as f:
         glossary = f.read().strip()
 
     ja_list = []
     for line in glossary.split("\n"):
-        ja = line.split(",")[0]
+        try:
+            ja = line.split(",")[0]
+            en = line.split(",")[1]
+        except IndexError:
+            print(f"❌  Did not find value for record: {ja}")
+            sys.exit(1)
+
         ja_list.append(ja)
 
     if duplicates := [k for k, v in Counter(ja_list).items() if v > 1]:
         print(f"❌  Duplicates found in glossary. Culprits:\n{duplicates}")
         sys.exit(1)
 
-    print("✔️  Glossary is good!")
+    print("✔️  No glossary duplicates found!")
 
 
 def check_jsons():
+    """Checks jsons for any syntax issues."""
     for file in os.listdir("json/"):
         with open(f"json/{file}", "r", encoding="utf-8") as f:
-            print(f"Opening file {file} for JSON validation.")
+            print(f"Opening file {file} to validate JSON.")
             try:
                 json.loads(f.read())
             except:
-                print(f"❌  {file} did not pass JSON validation.")
+                print(f"❌  {file} has invalid JSON.")
                 sys.exit(1)
-        print(f"✔️  {file} is good!")
+
+        print(f"✔️  {file} is valid JSON!")
 
 
 if __name__ == "__main__":
