3ecc4a6acffde78f82169ef01649e0811f5598cf
Shobu <84678540+xshobux@users.noreply.github.com>
2024-08-08 19:05:59 -0400

    more quest rewards (#259)
    Co-authored-by: xshobux <xshobux@users.noreply.github.com>

--- generate_glossary/ignore.py
+++ generate_glossary/ignore.py
@@ -165,5 +165,6 @@ IGNORE = {
     "白猫": "White Cat in Weblate, doesn't need capitalization",
     "下層": "Lower in Weblate, doesn't need captitalization",
     "城門": "Castle Gate in Weblate, doesn't need capitalization",
-    "リラ": "Lira in Weblate, too short a string"
+    "リラ": "Lira in Weblate, too short a string",
+    "魂の形": "Figure of a Spirit in Weblate, don't want to use this phrase"
 }
