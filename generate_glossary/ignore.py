66c01ee69e4a40493db1b494ba637c43702b452b
xshobux <84678540+xshobux@users.noreply.github.com>
2025-04-30 21:04:08 -0400

    Update ignore.py

--- generate_glossary/ignore.py
+++ generate_glossary/ignore.py
@@ -188,5 +188,6 @@ IGNORE = {
     "ごきげん": "Cheery in weblate, doesn't actually mean this word",
     "酒場": "Tavern in weblate, don't want this capitalized",
     "玉座": "Throne in weblate, don't want this capitalized",
-    "パン": "Bread in weblate, don't need this capitalized"
+    "パン": "Bread in weblate, don't need this capitalized",
+    "モモ": "Peach in weblate, don't need this capitalized"
 }
