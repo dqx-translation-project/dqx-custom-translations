d65ad5c3c8c60e810210baf604755f717367fcb7
xshobux <84678540+xshobux@users.noreply.github.com>
2025-06-26 00:20:35 -0400

    Remove Dragon from ignore.py
    Monster name not translating because of this

--- generate_glossary/ignore.py
+++ generate_glossary/ignore.py
@@ -182,7 +182,6 @@ IGNORE = {
     "ウサギ": "Rabbit in weblate, shouldn't be capitalized",
     "の砂漠": "Desert of in weblate, don't use",
     "おもて": "Heads, too short a word",
-    "ドラゴン": "Green Dragon in weblate, no",
     "ラッキー": "Drang in weblate, this is too common a word",
     "ルン": "Rune in weblate, too short",
     "ごきげん": "Cheery in weblate, doesn't actually mean this word",
